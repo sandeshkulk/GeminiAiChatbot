610e1cfefe6c35d3904c619c7fa6346fbb4487db
Sandesh Kulkarni <kulkarni12sandesh@gmail.com>
2024-06-10 12:04:38 +0100

    Home Screen and ViewModel

---
+++ app/src/main/java/com/example/geminiaichatbot/HomeViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.geminiaichatbot
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.BuildConfig
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.GenerationConfig
+import com.google.ai.client.generativeai.type.generationConfig
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+//import com.example.geminiaichatbot.BuildConfig
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class HomeViewModel: ViewModel() {
+
+    private val _uiState: MutableStateFlow<HomeUIState> = MutableStateFlow(HomeUIState.Intial)
+    val uiState =_uiState.asStateFlow()
+
+    private lateinit var generativeModel:GenerativeModel
+    init {
+        val config = generationConfig{
+            temperature=0.7f
+        }
+//        val generativeModel =GenerativeModel(
+//            modelName = "gemini-1.5-pro-latest",
+//            apiKey = BuildConfig.apiKey,
+//            generationConfig = config
+//        )
+    }
+    fun questioning(userInput:String, selectedImages:List<Bitmap>){
+       _uiState.value=HomeUIState.Loading
+        val prompt="Take a Look at images and then answer the question: $userInput"
+        viewModelScope.launch(Dispatchers.IO){
+            try{
+                val content= content {
+                    for(bitmap in selectedImages){
+                        image(bitmap)
+                    }
+                    text(prompt)
+                }
+                var output=""
+                generativeModel.generateContentStream(content).collect{
+                    output+=it.text
+                    _uiState.value=HomeUIState.Success(output)
+                }
+            } catch (e:Exception){
+                _uiState.value=HomeUIState.Error(e.localizedMessage?:"Error Generating Content")
+            }
+
+        }
+    }
+}
+
+sealed interface HomeUIState{
+    object Intial:HomeUIState
+    object Loading:HomeUIState
+    data class Success(val outputText:String):HomeUIState
+    data class Error(val errorText:String):HomeUIState
+}
--- app/src/main/java/com/example/geminiaichatbot/MainActivity.kt
+++ app/src/main/java/com/example/geminiaichatbot/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.geminiaichatbot.screen.AppContent
 import com.example.geminiaichatbot.ui.theme.GeminiAiChatbotTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GeminiAiChatbotTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppContent()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GeminiAiChatbotTheme {
-        Greeting("Android")
     }
 }
